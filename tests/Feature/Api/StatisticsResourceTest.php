1cb6ba6afb41a83baf8744f5c5226d8c2a696039
Robert Kummer <rok@ipunkt.biz>
2017-04-06 16:31:13 +0200

    statistics api tested

---
+++ tests/Feature/Api/StatisticsResourceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class StatisticsResourceTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+    /** @test */
+    public function it_can_fetch_the_statistics_resource()
+    {
+        // ARRANGE
+
+        // ACT
+        $response = $this->getJson('/api/statistics');
+
+        // ASSERT
+        $response->assertStatus(200)
+            ->assertHeader('Content-Type', 'application/vnd.api+json')
+            ->assertExactJson([
+                'data' => [
+                    'type' => 'statistics',
+                    'id' => 'statistics',
+                    'attributes' => [
+                        'size' => 0,
+                        'files' => 0,
+                        'aliases' => 0,
+                        'hits' => 0,
+                    ],
+                ]
+            ]);
+    }
+
+}
