c4ddaa394cbf8804a3403189a9ac5af425c952a0
Robert Kummer <github@mail.robert-kummer.de>
2017-04-07 11:49:17 +0000

    Apply fixes from StyleCI

--- app/Transformers/FileAliasTransformer.php
+++ app/Transformers/FileAliasTransformer.php
@@ -10,7 +10,7 @@ class FileAliasTransformer extends TransformerAbstract
     public function transform(FileAlias $model): array
     {
         return [
-            'id' => $model->proxyFile->reference . '.' . $model->getKey(),
+            'id' => $model->proxyFile->reference.'.'.$model->getKey(),
             'path' => $model->path,
             'valid_from' => $model->valid_from->toIso8601String(),
             'valid_until' => $model->valid_until === null ? null : $model->valid_until->toIso8601String(),
