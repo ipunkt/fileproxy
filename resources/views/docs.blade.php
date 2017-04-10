b54b3c68ac86103cd58486a42e5b6920897ef977
Robert Kummer <rok@ipunkt.biz>
2017-04-10 11:53:00 +0200

    documentation for fetching file and aliases

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -22,6 +22,10 @@
 		<div>
 			<p>The file proxy app serves files via aliases. So you have to create a file (via upload or remote url). After the
 		file exists you can create as many aliases as you like. Each can have different accessibility flags.</p>
+			<p>The main identification thing in file proxy is the returned file id (UUID4). This identifier you have to
+				remember for identification, creating aliases and fetching download hits.</p>
+			<p>There is no listing of serving files within the app. You have to remember in your source system or you
+				have to implement it in your local version. But we didn't recommend that.</p>
 		</div>
 	</div>
 
@@ -32,6 +36,7 @@
 	<div class="side-by-side">
 		<div>
 			<p>Request Body has following content for file upload.</p>
+			<p>The returned file id you have to remember.</p>
 		</div>
 		<div>
 			<p><strong><kbd>POST /api/files</kbd></strong></p>
@@ -55,7 +60,8 @@
       "filename": "file.ext",
       "size": 938,
       "checksum": "588c77c934d6a3b2f1d07973569ef5eb5779dd4f",
-      "mimetype": "text/html"
+      "mimetype": "text/html",
+      "hits": 0
     }
   }
 }</code></pre>
@@ -97,11 +103,86 @@
       "filename": "file.ext",
       "size": 0,
       "checksum": null,
-      "mimetype": null
+      "mimetype": null,
+      "hits": 0
     }
   }
 }</code></pre>
 
+		</div>
+	</div>
+
+	<h4>Fetching a files resource</h4>
+
+	<div class="side-by-side">
+		<div>
+			<p>You can retrieve file data like this.</p>
+		</div>
+		<div>
+			<p><strong><kbd>GET /api/files/d7a3913e-44a9-4aa6-ac8e-b9441cba07f8</kbd></strong></p>
+			<p>Response</p>
+			<pre><code>{
+  "data": {
+    "type": "files",
+    "id": "d7a3913e-44a9-4aa6-ac8e-b9441cba07f8",
+    "attributes": {
+      "filename": "file.ext",
+      "size": 938,
+      "checksum": "588c77c934d6a3b2f1d07973569ef5eb5779dd4f",
+      "mimetype": "text/html",
+      "hits": 12
+    }
+  }
+}</code></pre>
+		</div>
+	</div>
+
+	<h4>Fetching a files resource including aliases</h4>
+
+	<div class="side-by-side">
+		<div>
+			<p>You can retrieve file data with all existing aliases inclusively.</p>
+		</div>
+		<div>
+			<p><strong><kbd>GET /api/files/d7a3913e-44a9-4aa6-ac8e-b9441cba07f8?include=aliases</kbd></strong></p>
+			<p>Response</p>
+			<pre><code>{
+  "data": {
+    "type": "files",
+    "id": "d7a3913e-44a9-4aa6-ac8e-b9441cba07f8",
+    "attributes": {
+      "filename": "file.ext",
+      "size": 938,
+      "checksum": "588c77c934d6a3b2f1d07973569ef5eb5779dd4f",
+      "mimetype": "text/html",
+      "hits": 12
+    },
+    "relationships": {
+      "aliases": {
+        "data": [
+          {
+            "type": "aliases",
+            "id": "d7a3913e-44a9-4aa6-ac8e-b9441cba07f8.1",
+          },
+        ],
+      },
+    },
+  },
+  "included": [
+    {
+      "type": "aliases",
+      "id":  => "d7a3913e-44a9-4aa6-ac8e-b9441cba07f8.1",
+      "attributes": {
+        "path": "test.pdf",
+        "valid_from": "",
+        "valid_until": null,
+        "hits": 12,
+        "hits_left' => null,
+        "hits_total' => null,
+      },
+    }
+  ]
+}</code></pre>
 		</div>
 	</div>
 
