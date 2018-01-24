9481d3458263522ed885eb58d441f5b98cbad465
Robert Kummer <rok@ipunkt.biz>
2018-01-24 11:24:56 +0100

    Fixing "file_put_contents(): Exclusive locks are not supported for this stream" exception in docker on nfs file systems

--- app/Jobs/CreateLocalFile.php
+++ app/Jobs/CreateLocalFile.php
@@ -9,60 +9,63 @@ use Illuminate\Http\UploadedFile;
 
 class CreateLocalFile
 {
-    use Dispatchable;
+	use Dispatchable;
 
-    /**
-     * @var \Illuminate\Http\UploadedFile
-     */
-    private $file;
+	/**
+	 * @var \Illuminate\Http\UploadedFile
+	 */
+	private $file;
 
-    /**
-     * @var string
-     */
-    private $reference;
+	/**
+	 * @var string
+	 */
+	private $reference;
 
-    public function __construct(string $reference, UploadedFile $file)
-    {
-        $this->reference = $reference;
-        $this->file = $file;
-    }
+	public function __construct(string $reference, UploadedFile $file)
+	{
+		$this->reference = $reference;
+		$this->file = $file;
+	}
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-        $fileHandle = $this->file->openFile();
-        $content = $fileHandle->fread($this->file->getSize());
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		$fileHandle = $this->file->openFile();
+		$content = $fileHandle->fread($this->file->getSize());
 
-        $proxyFile = new ProxyFile([
-            'reference' => $this->reference,
-            'type' => 'local',
-            'filename' => $this->file->getClientOriginalName(),
-            'mimetype' => $this->file->getClientMimeType(),
-            'size' => $this->file->getSize(),
-            'checksum' => sha1($content),
-        ]);
-        $proxyFile->save();
+		$proxyFile = new ProxyFile([
+			'reference' => $this->reference,
+			'type' => 'local',
+			'filename' => $this->file->getClientOriginalName(),
+			'mimetype' => $this->file->getClientMimeType(),
+			'size' => $this->file->getSize(),
+			'checksum' => sha1($content),
+		]);
+		$proxyFile->save();
 
-        /** @var \App\LocalFile $localFile */
-        $localFile = $proxyFile->localFile()->create([
-            'path' => uniqid('', true),
-        ]);
+		/** @var \App\LocalFile $localFile */
+		$localFile = $proxyFile->localFile()->create([
+			'path' => uniqid('', true),
+		]);
 
-        /** @var Filesystem|\Illuminate\Filesystem\FilesystemAdapter $filesystem */
-        $filesystem = app(Filesystem::class);
+		/** @var Filesystem|\Illuminate\Filesystem\FilesystemAdapter $filesystem */
+		$filesystem = app(Filesystem::class);
 
-        if (! $filesystem->exists('local')) {
-            $filesystem->makeDirectory('local');
-        }
+		if (!$filesystem->exists('local')) {
+			$filesystem->makeDirectory('local');
+		}
 
-        $path = $localFile->getLocalStoragePath();
-        if ($filesystem->put($path, $content)) {
-            $localFile->path = $path;
-            $localFile->save();
-        }
-    }
+		$path = $localFile->getLocalStoragePath();
+
+		$fh = fopen($this->file->getRealPath(), 'rb');
+		if ($filesystem->put($path, $fh)) {
+			$localFile->path = $path;
+			$localFile->save();
+		}
+		fclose($fh);
+	}
 }
